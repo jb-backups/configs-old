908068d861babd1eab7fd8528837d5f447e55596
Jonathan Bergson <jonathan@bergson.me>
2019-05-30 23:20:48 -0300

    feat(shell): add shell snippets

---
+++ shell/snippets.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+source $(dirname $0)/snippets/clear.sh
+source $(dirname $0)/snippets/docker.sh
+source $(dirname $0)/snippets/editor.sh
+source $(dirname $0)/snippets/git.sh
+source $(dirname $0)/snippets/gitflow.sh
+source $(dirname $0)/snippets/homestead.sh
+source $(dirname $0)/snippets/node.sh
+source $(dirname $0)/snippets/npm.sh
+source $(dirname $0)/snippets/outer.sh
+source $(dirname $0)/snippets/vagrant.sh
+source $(dirname $0)/snippets/yarn.sh
+
+mybash() source ~/bash/bash.sh;
+
+# echo "My shell snippets is loaded!"
---
+++ shell/snippets/clear.sh
@@ -0,0 +1,9 @@
+cealr() clear;
+cearl() clear;
+celar() clear;
+ckear() clear;
+claer() clear;
+clera() clear;
+lcear() clear;
+
+clar() clear;
