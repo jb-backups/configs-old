7ab896e7be05221771f7d1e7298bd080b0b8d3cf
Jonathan Bergson <jonathan@track.co>
2021-10-07 10:14:46 -0300

    fix: remove command

--- settings/shell/trackco.sh
+++ settings/shell/trackco.sh
@@ -3,4 +3,4 @@ cdf() cd ~/www/a2/frontend/track;
 cdd() cd ~/www/design-system;
 cdui() cd ~/www/track-ui;
 
-runb() cd ~/www/a2/backend/cmd/core && go run main.go;
+# runb() cd ~/www/a2/backend/cmd/core && go run main.go;
