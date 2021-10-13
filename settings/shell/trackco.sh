b06bd626bd16a806febad2ffba0957668ee0d1cd
Jonathan Bergson <jonathan@track.co>
2021-10-13 16:38:40 -0300

    update

--- settings/shell/trackco.sh
+++ settings/shell/trackco.sh
@@ -1,6 +1,6 @@
-cdb() cd ~/www/a2/backend/cmd/core;
-cdf() cd ~/www/a2/frontend/track;
-cdd() cd ~/www/design-system;
+cdb() cd ~/www/track/a2/backend/cmd/core;
+cdf() cd ~/www/track/a2/frontend/track;
+cdd() cd ~/www/track/design-system;
 cdui() cd ~/www/track-ui;
 
 # runb() cd ~/www/a2/backend/cmd/core && go run main.go;
